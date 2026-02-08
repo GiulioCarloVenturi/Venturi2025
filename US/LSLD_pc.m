763abe7758b07bf281b5b8d67a77865a34e1294f
Giulio Carlo Venturi <159551033+GiulioCarloVenturi@users.noreply.github.com>
2026-02-08 13:55:50 +0100

    Add files via upload

---
+++ US/LSLD_pc.m
@@ -0,0 +1,31 @@
+%% Compute Loan Demand and Supply
+function [LS, LD, rho_grid]=LSLD(rho_grid,func_var,y)
+mu_d = func_var.mu_d;
+rq_ratio = func_var.rq_ratio;
+cost = func_var.cost;
+beta = func_var.beta;
+df_inv = func_var.df_inv;
+r_reserves = func_var.r_reserves;
+xi_func = @(rho) (1-rq_ratio)*max(1+rho,1+r_reserves)+rq_ratio.*(1+r_reserves)-cost;
+D_bar = max(y(:,1));
+psi_func = @(x) interp1(y(:,1),y(:,3),min(x,D_bar)).*(x<=D_bar)+beta.*(x>D_bar);
+dpsi_func = @(x) interp1(y(:,1),y(:,4),min(x,D_bar)).*(x<=D_bar);
+%eq_D_func = @(x,y) xi_func(y).*(psi_func(x)./((1-mu_d).*(1-dpsi_func(x).*x/Nb)) )-1;
+eq_D_func = @(x,y) xi_func(y) - (1-mu_d)./psi_func(x);
+
+%% Compute Loan Supply for equation 12 case 3: R_r < R_l < 1/beta
+if rho_grid(1) < r_reserves
+    ID = find(rho_grid > r_reserves,1,'first');
+    rho_grid = [rho_grid(1:ID-1),r_reserves,rho_grid(ID:end)];
+end
+for i=1:length(rho_grid)
+    [D_grid(i),fval(i)] = fzero(@(x) eq_D_func(x,rho_grid(i)), [1e-4,D_bar]);
+    LS(i) = (1-rq_ratio)*D_grid(i)*psi_func(D_grid(i));
+end
+%% Compute Loan Supply for equation 12 case 3: R_l < R_r
+if rho_grid(1) < r_reserves
+    ID = find(rho_grid < r_reserves,1,'last');
+    LS(1:ID)=0;
+end
+%% Compute Loan Demand equation, page 17
+LD = df_inv(rho_grid);
---
+++ US/SS_eq_noCBDC_fee.m
@@ -0,0 +1,37 @@
+function eq=SS_eq_noCBDC_cash(func_var,Nb,y)
+%% Parameters
+rq_ratio = func_var.rq_ratio;
+cost = func_var.cost;
+rho_bar= func_var.rho_bar;
+beta = func_var.beta;
+df_inv = func_var.df_inv;
+df = func_var.df;
+r_reserves = func_var.r_reserves;
+mu_db = func_var.mu_db;
+%% Functions
+xi_func = @(rho) (1-rq_ratio).*max(1+rho,1+r_reserves) + rq_ratio*(1+r_reserves) - cost;
+D_bar = max(y(:,1));
+Z_bar = max(y(:,2));
+psi_func = @(x) interp1(y(:,1),y(:,3),min(x,D_bar)).*(x<=D_bar)+beta.*(x>D_bar);
+dpsi_func = @(x) interp1(y(:,1),y(:,4),min(x,D_bar)).*(x<=D_bar);
+z_func = @(x) interp1(y(:,1),y(:,2),min(x,Z_bar));
+%% Loan Supply and Demand at Lowest R_L
+Dlow = fzero(@(x) xi_func(r_reserves).*(psi_func(x)./((1-mu_db).*(1-dpsi_func(x).*x/Nb)) )-1,[0,10]);
+LS_low = (1-rq_ratio)*Dlow*psi_func(Dlow);
+LD_high = df_inv(r_reserves);
+%% Loan Supply and Demand at Highest R_L
+Dbar = fzero(@(x) xi_func(rho_bar).*(psi_func(x)./((1-mu_db).*(1-dpsi_func(x).*x/Nb)) )-1,[0,D_bar]);
+LS_bar = (1-rq_ratio)*Dbar*psi_func(Dbar);
+LD_low = df_inv(rho_bar);
+%% Loan Supply and Demand at R_L
+rho_func = @(x) df(x)-1; 
+eq_D_func = @(x) xi_func(rho_func(psi_func(x).*x*(1-rq_ratio))).*(psi_func(x)./((1-mu_db).*(1-dpsi_func(x).*x/Nb)) )-1;
+
+if LS_low>LD_high
+    eq = [z_func(Dlow),Dlow,r_reserves];
+elseif LS_bar<LD_low
+    eq = [z_func(Dbar),Dbar,rho_bar];
+else
+    D = fzero(@(x) eq_D_func(x),[Dlow,Dbar]);
+    eq = [z_func(D),D,rho_func(D*psi_func(D)*(1-rq_ratio))];
+end
